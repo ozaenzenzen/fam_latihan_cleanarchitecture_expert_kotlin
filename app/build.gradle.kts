011dd9fb8187625d6e2f4036349beb85e1a38023
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-07-27 14:06:47 +0700

    materi clean architecture + unit test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    testImplementation("org.mockito:mockito-core:4.4.0")
+    testImplementation("org.mockito:mockito-inline:4.4.0")
 }
